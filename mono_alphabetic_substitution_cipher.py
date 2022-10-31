0a12f654b930f7a47dac3122c06d8127c337ba96
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2022-10-31 16:21:32 +0530

    All Ciphers

---
+++ mono_alphabetic_substitution_cipher.py
@@ -0,0 +1,9 @@
+# Implement mono alphabetic substitution cipher
+plaintext = input("Enter the text: ")
+abc = "abcdefghijklmnopqrstuvwxyz"
+encrypt = "".join([abc[(abc.find(c) + 13)%26] for c in plaintext])
+decrypt = "".join([abc[(abc.find(c) + 13)%26] for c in encrypt])
+
+print("The original text is " + plaintext)
+print("The encrypted text is " + encrypt)
+print("The decrypted text is " + decrypt)
